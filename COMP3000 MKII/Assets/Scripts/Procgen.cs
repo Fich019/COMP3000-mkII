70fa7d48f2d6a882a4448829b69f0f6755b0d824
Zack Hawkins <zackh019@gmail.com>
2021-05-28 22:19:30 +0100

    Script modifications for gif

--- COMP3000 MKII/Assets/Scripts/Procgen.cs
+++ COMP3000 MKII/Assets/Scripts/Procgen.cs
@@ -34,6 +34,9 @@ public class Procgen : MonoBehaviour
     public GameObject playerSpawner;
     public GameObject objects;
 
+    public int GlobX; //For gif generation version
+    public int GlobY;
+
     public RawImage visulizationUI;
     private Texture2D noiseTexture;
 
@@ -45,9 +48,10 @@ public class Procgen : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        citygrid = new int[perlinGridStepSizeX, perlinGridStepSizeY];
-        
 
+        citygrid = new int[perlinGridStepSizeX, perlinGridStepSizeY];
+
+        
         offestX = Random.Range(0, 99999);
         offsetY = Random.Range(0, 99999);
 
@@ -68,8 +72,9 @@ public class Procgen : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        //StepVisualizePrefabs(GlobX); //Gif generation version
     }
+
 
     private void Awake()
     {
@@ -83,6 +88,7 @@ public class Procgen : MonoBehaviour
         }
     }
 
+
     public void Generate()
     {
         GenerateTexture();
@@ -93,6 +99,8 @@ public class Procgen : MonoBehaviour
         }
 
     }
+
+
     void GenerateTexture()
     {
         noiseTexture = new Texture2D(width, height);
@@ -113,6 +121,7 @@ public class Procgen : MonoBehaviour
 
     }
 
+
     Color CalculateColour(int x, int y)
     {
         float xCoord = (float)x / width * scale + offestX;
@@ -124,6 +133,7 @@ public class Procgen : MonoBehaviour
         return new Color(sample, sample, sample);
     }
 
+
     void VisualizePrefabs()
     {
         GameObject objectParent = new GameObject("ObjectParent");
@@ -134,6 +144,8 @@ public class Procgen : MonoBehaviour
             for (int y = 0; y < perlinGridStepSizeY -1; y++)
             {
 
+                //ExecuteAfterTime();
+                //System.Threading.Thread.Sleep(3000);
                 int ranObjects = Random.Range(0,20);
 
                 // if (citygrid[x,] )
@@ -208,7 +220,7 @@ public class Procgen : MonoBehaviour
                     GameObject spawnS = Instantiate(carspawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetVert.transform.rotation * Quaternion.Euler(0f, 0f, 180f));
                     spawnS.transform.SetParent(objectParent.transform);
                 }
-                else if (citygrid[x,y] == -9)
+                else if (citygrid[x, y] == -9)
                 {
                     GameObject spawnV = Instantiate(playerSpawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, playerSpawner.transform.rotation);
                     spawnV.transform.SetParent(objectParent.transform);
@@ -251,11 +263,155 @@ public class Procgen : MonoBehaviour
                     //Instantiate(spawn);
                 }
                 
-
+                
             }
+            
         }
 
         objectParent.transform.position = new Vector3(-perlinGridStepSizeX * .5f, -1 * .5f, -perlinGridStepSizeY * .5f);
+    }
+
+
+    public void StepVisualizePrefabs(int x)
+    {
+        GameObject objectParent = new GameObject("ObjectParent");
+        objectParent.transform.SetParent(this.transform);
+        for (int y = 0; y < perlinGridStepSizeY - 1; y++)
+        {
+
+            if (x <= perlinGridStepSizeX - 1 && y <= perlinGridStepSizeY - 1)
+            {
+
+                //ExecuteAfterTime();
+                //System.Threading.Thread.Sleep(3000);
+                int ranObjects = Random.Range(0, 20);
+
+                // if (citygrid[x,] )
+                if (citygrid[x, y] == -1)
+                {
+                    //spawn street here
+                    GameObject spawnH = Instantiate(streetHor, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetHor.transform.rotation);
+                    spawnH.transform.SetParent(objectParent.transform);
+
+                    GameObject spawnS = Instantiate(lamp, new Vector3(x + 0.74f, 0.6f, y + 1f) + transform.position, lamp.transform.rotation);
+                    spawnS.transform.SetParent(objectParent.transform);
+
+                    if (ranObjects == 1)
+                    {
+                        GameObject spawnO = Instantiate(objects, new Vector3(x + 1f, Random.Range(3, 7), y + 1f) + transform.position, objects.transform.rotation);
+                        spawnO.transform.SetParent(objectParent.transform);
+                    }
+                    //if (count == 3)
+                    //{
+                    //    GameObject spawnS = Instantiate(lamp, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, lamp.transform.rotation);
+                    //    spawnS.transform.SetParent(objectParent.transform);
+                    //    count = 0;
+                    //}
+                    //Debug.Log("pp");
+                }
+                else if (citygrid[x, y] == -2)
+                {
+                    //spawn street here
+                    GameObject spawnV = Instantiate(streetVert, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetVert.transform.rotation);
+                    spawnV.transform.SetParent(objectParent.transform);
+
+                    GameObject spawnS = Instantiate(lamp, new Vector3(x + 1f, 0.6f, y + 1.3f) + transform.position, transform.rotation * Quaternion.Euler(0f, 90f, 0f));
+                    spawnS.transform.SetParent(objectParent.transform);
+
+                    if (ranObjects == 1)
+                    {
+                        GameObject spawnO = Instantiate(objects, new Vector3(x + 1f, Random.Range(3, 7), y + 1f) + transform.position, objects.transform.rotation);
+                        spawnO.transform.SetParent(objectParent.transform);
+                    }
+                    //Debug.Log("pp");
+                }
+                else if (citygrid[x, y] == -3)
+                {
+                    //spawn street here
+                    GameObject spawnC = Instantiate(streetCross, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetCross.transform.rotation);
+                    spawnC.transform.SetParent(objectParent.transform);
+
+                    if (ranObjects == 1)
+                    {
+                        GameObject spawnO = Instantiate(objects, new Vector3(x + 1f, Random.Range(3, 7), y + 1f) + transform.position, objects.transform.rotation);
+                        spawnO.transform.SetParent(objectParent.transform);
+                    }
+                    //Debug.Log("pp");
+                }
+                else if (citygrid[x, y] == -6)
+                {
+                    GameObject spawnS = Instantiate(carspawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetHor.transform.rotation);
+                    spawnS.transform.SetParent(objectParent.transform);
+                }
+                else if (citygrid[x, y] == -7)
+                {
+                    GameObject spawnS = Instantiate(carspawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetVert.transform.rotation * Quaternion.Euler(0f, 0f, 0f));
+                    spawnS.transform.SetParent(objectParent.transform);
+                }
+                else if (citygrid[x, y] == -11)
+                {
+                    GameObject spawnS = Instantiate(carspawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetHor.transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+                    spawnS.transform.SetParent(objectParent.transform);
+                }
+                else if (citygrid[x, y] == -12)
+                {
+                    GameObject spawnS = Instantiate(carspawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetVert.transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+                    spawnS.transform.SetParent(objectParent.transform);
+                }
+                //else if (citygrid[x,y] == -9)
+                //{
+                //    GameObject spawnV = Instantiate(playerSpawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, playerSpawner.transform.rotation);
+                //    spawnV.transform.SetParent(objectParent.transform);
+                //}
+                //else if (citygrid[x, y] == -13)
+                //{
+                //    GameObject spawnS = Instantiate(carspawner, new Vector3(x + 1f, 0.6f, y + 1f) + transform.position, streetVert.transform.rotation * Quaternion.Euler(0f, 0f, 180f));
+                //    spawnS.transform.SetParent(objectParent.transform);
+                //}
+                else
+                {
+                    Color check = CalculateColour(x, y);
+
+                    //if (check.g < 0.4f)
+                    //{
+                    //    int ranSelect = Random.Range(0, Prefabs.Length);
+
+                    //    GameObject spawn = Instantiate(Prefab, new Vector3(x, SampleStepped(x,y) * heightScale, y) + transform.position, transform.rotation);
+                    //    int i = Random.Range(0, 20);
+
+                    //    spawn.transform.SetParent(objectParent.transform);
+                    //    if (i == 1) { GameObject spawn2 = Instantiate(Prefabs[ranSelect], (new Vector3(x, Random.Range(1,3), y) - new Vector3(0,0.5f,0)) + transform.position, transform.rotation); spawn2.transform.SetParent(objectParent.transform); }
+                    //}
+
+                    int ranSelect = Random.Range(0, Prefabs.Length);
+                    float sample = SampleStepped(x, y);
+
+                    GameObject spawn = Instantiate(Prefab, new Vector3(x + 1f, sample * heightScale - 6f, y + 1f) + transform.position, transform.rotation);
+                    spawn.transform.SetParent(objectParent.transform);
+                    int i = Random.Range(0, 40);
+
+                    //if (i == 1)
+                    //{
+                    //    GameObject spawn2 = Instantiate(Prefabs[ranSelect], (new Vector3(x, (sample * heightScale) + Random.Range(1, 3), y) - new Vector3(0, 0.5f, 0)) + transform.position, transform.rotation);
+                    //    spawn2.transform.SetParent(objectParent.transform);
+                    //}
+
+
+
+                    //Instantiate(spawn);
+                }
+
+
+            }
+        
+            int gfunk = 0;
+            float Newtime = 3.00f;
+            for (float i = Newtime; i >= 0; i -= Time.deltaTime) { gfunk++; Debug.Log(gfunk); }
+        }
+
+        objectParent.transform.position = new Vector3(-perlinGridStepSizeX * .5f, -1 * .5f, -perlinGridStepSizeY * .5f);
+        GlobX = x + 1;
+        
     }
 
 
@@ -296,6 +452,7 @@ public class Procgen : MonoBehaviour
 
     }
 
+
     void GenerateStreetsX()
     {
         int r = numOfBuildingsBet;
@@ -333,6 +490,7 @@ public class Procgen : MonoBehaviour
 
     }
 
+
     public float SampleStepped(int x, int y)
     {
         int gridStepSizeX = Mathf.FloorToInt(width / perlinGridStepSizeX);
@@ -341,6 +499,7 @@ public class Procgen : MonoBehaviour
         float sampleFloat = noiseTexture.GetPixel((Mathf.FloorToInt(x * gridStepSizeX)), (Mathf.FloorToInt(y * gridStepSizeY))).grayscale;
         return sampleFloat;
     }
+
 
     public int GetFactor()
     {
@@ -364,6 +523,7 @@ public class Procgen : MonoBehaviour
         return chosenFac;
 
     }
+
 
     public int GetBuildingBetween()
     {
